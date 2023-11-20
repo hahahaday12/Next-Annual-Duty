cb07eee8d039193e3f9e855615ae94f144504573
hahahaday12 <hahahaday12@naver.com>
2023-11-21 02:58:01 +0900

    Feat: 로그인 페이지

---
+++ src/app/ui/login.tsx
@@ -0,0 +1,20 @@
+'use client';
+import Image from 'next/image';
+import { SigninForm } from '@/app/(form)/Signin';
+
+export const SignIn = () => {
+  return (
+    <div className="w-[568px] h-full bg-white absolute top-0 bottom-0 left-0 right-0 flex flex-col items-center justify-center m-auto">
+      <Image
+        src="/main.png"
+        alt="mainImage"
+        className="top-[70px] absolute z-10"
+        width={400}
+        height={400}
+      />
+      <div className="w-[400px] h-[500px] absolute bottom-[30px]">
+        <SigninForm />
+      </div>
+    </div>
+  );
+};
