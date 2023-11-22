556f7941225be82f3909b82ec43e2083946f0d97
hahahaday12 <hahahaday12@naver.com>
2023-11-23 03:07:37 +0900

    Feat: 카테고리 텍스트

---
+++ src/constants/headerLinks.ts
@@ -0,0 +1,10 @@
+export const links = [
+  { href: '/main/home', name: '    홈    ' },
+  { href: '/main/profile', name: '    프로필 수정    ' },
+  { href: '/main/schedule', name: '    내 일정보기    ' },
+  { href: '/main/application', name: '    연차/당직 신청    ' },
+];
+
+export const headerText = {
+  signout: '로그아웃'
+}
