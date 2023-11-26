c3d0e30448ec9aabd860e3b1ae33f67909250046
hahahaday12 <hahahaday12@naver.com>
2023-11-26 23:24:06 +0900

    Feat: 이미지 네임 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 import Image from 'next/image';
 import { SignIn } from './(form)/formSignin';
+import BackgroundImage from '@/assets/bgblue.png'
 
 export default function Home() {
   return (
     <main>
       <Image
-        src="/bgblue.png"
+        src={BackgroundImage}
         alt="backgroundImage"
         className="w-full h-screen md:max-md"
         width={1000}
