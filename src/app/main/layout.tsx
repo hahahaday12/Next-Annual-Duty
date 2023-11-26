294e9a105dde5cb66f972f85d9cc550c3a6afe55
hahahaday12 <hahahaday12@naver.com>
2023-11-26 23:28:30 +0900

    Feat: 메인 홈 페이지 공통 레이아웃

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -1,9 +1,10 @@
 import { Header } from '@/components/header';
 
+
 export default function Layout({ children }: { children: React.ReactNode }) {
 
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-slate-500">
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-[#f5f6fa]">
       <div className="w-full flex-none md:w-64">
         <Header />
       </div>
