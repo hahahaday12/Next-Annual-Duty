95d79dc7adce1fcec0f04e30934b5ec54b45791e
hahahaday12 <hahahaday12@naver.com>
2023-11-28 23:54:12 +0900

    Feat: 공통 헤더 컴포넌트

---
+++ src/components/common/header.tsx
@@ -0,0 +1,66 @@
+'use client';
+import { links, headerText } from '@/constants/headerLinks';
+import Image from 'next/image';
+import { usePathname, useRouter } from 'next/navigation';
+import Link from 'next/link';
+import clsx from 'clsx';
+import TitleLogo from '@/assets/titlelogo.png';
+import { HeaderProfile } from '../profile/headerProfile';
+
+export interface InfoResponse extends Response {
+  response: {
+    email: string;
+    hireDate: string;
+    profileImage: string;
+    remainVacation: number;
+    usedVacation: number;
+    username: string;
+  };
+}
+
+export const Header = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const signOut = () => {
+    localStorage.removeItem('token');
+    router.push('/main/home');
+  };
+
+  return (
+    <>
+      <div className="flex justify-center items-center w-full h-[100px] fixed z-[200] shadow-[0_4px_6px_rgba(0,0,0,0.4)] bg-white">
+        <div className="w-[1080px] h-[100px] flex items-center content-between bg-[#A9B388]">
+          <div className="flex items-center font-LINESeedKRBd">
+            <Image
+              src={TitleLogo}
+              alt="titlelogo"
+              width={100}
+              height={100}
+              className="w-[210px] h-[80px] mr-[40px] cursor-pointer bg-white"
+              onClick={() => router.push('/main/home')}
+            />
+            {links.map((link) => {
+              return (
+                <Link
+                  key={link.name}
+                  href={link.href}
+                  className={clsx(
+                    'text-[#000000] mr-[70px] whitespace-pre bg-slate-600',
+                    {
+                      'text-[#2656f6] whitespace-pre decoration-4 underline-offset-[44px] underline':
+                        pathname === link.href,
+                    }
+                  )}
+                >
+                  {link.name}
+                </Link>
+              );
+            })}
+          </div>
+          <HeaderProfile headerText={headerText} />
+        </div>
+      </div>
+    </>
+  );
+};
