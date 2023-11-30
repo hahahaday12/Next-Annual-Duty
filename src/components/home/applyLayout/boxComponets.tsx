ddd99571d2a33eb7320b832d158ab40aa03d4467
hahahaday12 <hahahaday12@naver.com>
2023-12-01 01:34:52 +0900

    Style: 연차, 당직 공통 box style 생성

---
+++ src/components/home/applyLayout/boxComponets.tsx
@@ -0,0 +1,11 @@
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+export function ApplyInfobox({ children, className }: ButtonProps) {
+  return (
+    <div className="w-[518px] h-[266px] rounded-[10px] bg-[#ffff] pb-[50px] shadow-[1px_2px_7px_1px_#50515985]">
+      {children}
+    </div>
+  );
+}
