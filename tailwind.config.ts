b7d2331e6e5e6454571efbae430f60dae23ee7ba
hahahaday12 <hahahaday12@naver.com>
2023-12-03 22:35:00 +0900

    Feat: 폰트 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,15 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-
       colors: {
         'primary-Blue': '#2656f6',
       },
+    },
+    fontFamily: {
+      LINESeedKRBd: ['LINESeedKR-Bd'],
     },
   },
   plugins: [],
