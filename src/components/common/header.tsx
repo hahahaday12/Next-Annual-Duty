b77158ee3db83cc970bd41b3c46ad9c83a25f566
hahahaday12 <hahahaday12@naver.com>
2023-12-03 22:39:32 +0900

    Style: 공통 헤더 이미지 형식 변환

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
 import Link from 'next/link';
 import clsx from 'clsx';
-import TitleLogo from '@/assets/titlelogo.png';
+import TitleLogo from '@/assets/service-title.avif';
 import { HeaderProfile } from '../profile/headerProfile';
 
 export interface InfoResponse extends Response {
@@ -22,23 +22,18 @@ export const Header = () => {
   const router = useRouter();
   const pathname = usePathname();
 
-  const signOut = () => {
-    localStorage.removeItem('token');
-    router.push('/main/home');
-  };
-
   return (
     <>
-      <div className="flex justify-center items-center w-full h-[100px] fixed z-[200] shadow-[0_4px_6px_rgba(0,0,0,0.4)] bg-white">
+      <div className="flex justify-center items-center w-full h-[100px] fixed z-[200] shadow-[0_4px_6px_rgba(0,0,0,0.4)] bg-white overflow-hidden">
         <div className="w-[1080px] h-[100px] flex items-center content-between">
           <div className="flex items-center font-LINESeedKRBd">
             <Image
               src={TitleLogo}
               alt="titlelogo"
-              width={100}
+              width={200}
               height={100}
               priority
-              className="w-[210px] h-[80px] mr-[40px] cursor-pointer bg-white"
+              className="mr-[40px] mb-[50px] cursor-pointer"
               onClick={() => router.push('/main/home')}
             />
             {links.map((link) => {
