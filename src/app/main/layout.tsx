d19f19040305ca7c07b4bd72eab1c4360947efe6
hahahaday12 <hahahaday12@naver.com>
2023-12-03 22:35:25 +0900

    Style: 페이지 스타일 변경

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -2,8 +2,8 @@ import { Header } from '@/components/common/header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row  bg-[#f5f6fa]">
-      <div className="w-full flex-none md:w-64">
+    <div className="flex h-screen flex-col bg-[#f5f6fa]">
+      <div className="w-full flex-none">
         <Header />
       </div>
       <div className="flex-grow p-6 ">{children}</div>
