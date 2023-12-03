eef8d52b4a6a9a4ee55b63da8c3994d936a997d4
hahahaday12 <hahahaday12@naver.com>
2023-12-03 22:38:14 +0900

    Style: 스타일 추가,  이미지 변수화

--- src/app/(form)/formSignin.tsx
+++ src/app/(form)/formSignin.tsx
@@ -1,19 +1,18 @@
 import Image from 'next/image';
 import { SigninForm } from '@/components/form/Signin';
+import MainImage from '@/assets/main.avif';
 
 export const SignIn = () => {
   return (
-    <div className="w-[568px] h-full bg-white absolute top-0 bottom-0 left-0 right-0 flex flex-col items-center justify-center m-auto">
+    <div className="w-[568px] h-[500px] m-auto relative top-[-150px] bottom-0 left-0 right-0 flex flex-col items-center content-center bg-slate-600">
       <Image
-        src="/main.png"
+        src={MainImage}
         alt="mainImage"
-        className="top-[70px] absolute z-10"
+        className="top-[-350px] z-10"
         width={400}
         height={400}
       />
-      <div className="w-[400px] h-[500px] absolute bottom-[30px]">
-        <SigninForm />
-      </div>
+      <SigninForm />
     </div>
   );
 };
