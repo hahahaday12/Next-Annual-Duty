2a748a33ae39865fd59034c55ed0fbaa33afa26f
hahahaday12 <hahahaday12@naver.com>
2023-12-06 01:18:18 +0900

    Style: 스타일 수정

--- src/components/home/buttonBox/button/barButton.tsx
+++ src/components/home/buttonBox/button/barButton.tsx
@@ -3,7 +3,7 @@ import { commonTexts } from '@/constants/commonTexts';
 export const CommonBarButton = () => {
   return (
     <>
-      <div className="w-[100px] h-[13px] rounded-[30px] bg-[#190482] relative">
+      <div className="w-[100px] h-[13px] rounded-[30px] bg-[#190482]">
         <p className="w-[50px] ml-[110px]">{commonTexts.annualText}</p>
       </div>
       <div className="w-[100px] h-[13px] rounded-[30px] bg-[#7752fe] relative mt-[10px]">
