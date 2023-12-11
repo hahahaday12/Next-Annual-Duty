19bb1ccdd9995e9124719f3a5a33c2bed8f94c81
hahahaday12 <hahahaday12@naver.com>
2023-12-11 15:42:42 +0900

    Feat: 캘린더 공통 컴포넌트

--- src/components/common/calendar.tsx
+++ src/components/common/calendar.tsx
@@ -1,26 +1,22 @@
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import { useState, useRef } from 'react';
-import { CustomEvent } from '@/components/styleCommon/styleCommon';
-import { Item } from '@/hooks/useCalendarData';
+import { useRef } from 'react';
+import { AnnualDrowItem, DutyItemType } from '../application/dayApply';
+import { CombinedDataItem } from '@/hooks/useCalendarData';
 
-type Event = {
-  _def: any;
-  title: string;
-};
+interface CommonCalendarProps {
+  viewDrow: CombinedDataItem[];
+  CalDate: number;
+  setCalDate: (value: number) => void;
+}
 
-export const CalendarCommon = ({ viewDrow }: { viewDrow: Item[] }) => {
-  const [CalDate, setCalDate] = useState<number>(2023);
+export const CommonCalendar = ({
+  viewDrow,
+  CalDate,
+  setCalDate,
+}: CommonCalendarProps) => {
   const calendarRef = useRef<FullCalendar | null>(null);
-
-  const eventContent = ({ event }: { event: Event }) => {
-    return (
-      <CustomEvent title={event._def.extendedProps.type}>
-        {event.title}
-      </CustomEvent>
-    );
-  };
 
   const handleDatesSet = () => {
     if (calendarRef.current) {
@@ -34,20 +30,17 @@ export const CalendarCommon = ({ viewDrow }: { viewDrow: Item[] }) => {
   };
 
   return (
-    <div className="w-full h-[870px] pb-[40px] bg-[#ffff] relative top-[30px] rounded-[10px] shadow-[1px_2px_7px_1px_rgba(0,0,0,0.3)]">
-      <div className=" w-[1025px] relative m-auto h-[700px] top-[10px] font-LINESeedKR-Bd pb-[40px]">
-        <FullCalendar
-          plugins={[dayGridPlugin, interactionPlugin]}
-          initialView="dayGridMonth"
-          timeZone="Asia/Seoul"
-          eventContent={eventContent}
-          datesSet={handleDatesSet}
-          ref={calendarRef}
-          dayMaxEvents={true}
-          events={viewDrow as unknown as EventInit[]}
-          locale={'ko'}
-        />
-      </div>
-    </div>
+    <>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        timeZone="Asia/Seoul"
+        datesSet={handleDatesSet}
+        ref={calendarRef}
+        dayMaxEvents={true}
+        events={viewDrow as unknown as EventInit[]}
+        locale={'ko'}
+      />
+    </>
   );
 };
