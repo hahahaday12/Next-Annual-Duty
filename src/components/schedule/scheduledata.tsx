f675b56afca08be4aa3c7b5766209b9ece859e04
hahahaday12 <hahahaday12@naver.com>
2023-12-11 15:45:09 +0900

    Feat: 내일정 페이지 컴포넌트 수정

--- src/components/schedule/scheduledata.tsx
+++ src/components/schedule/scheduledata.tsx
@@ -1,26 +1,39 @@
 'use client';
-import { useState } from 'react';
+
 import { MyAnnualList, MyDutyList } from '../api/allApis';
 import { getMyTitleWithStatus } from '@/constants/customStatus';
+import { CommonCalendar } from '@/components/common/calendar';
+import { useMemo, useState } from 'react';
 import { useCalendarData } from '@/hooks/useCalendarData';
-import useCalendar from '@/hooks/useCalendarLayout';
 
 export const Schedule = () => {
-  const [CalDate] = useState<number>(2023);
+  const [CalDate, setCalDate] = useState<number>(2023);
 
-  const { viewDrow } = useCalendarData(
-    MyAnnualList(CalDate.toString()),
-    MyDutyList(CalDate.toString()),
-    getMyTitleWithStatus,
-    CalDate
+  const fetchDataFunction1 = useMemo(
+    () => MyAnnualList(CalDate.toString()),
+    [CalDate]
   );
 
-  const { CalendarComponent } = useCalendar({ viewDrow });
+  const fetchDataFunction2 = useMemo(
+    () => MyDutyList(CalDate.toString()),
+    [CalDate]
+  );
+
+  const { viewDrow } = useCalendarData(
+    fetchDataFunction1,
+    fetchDataFunction2,
+    getMyTitleWithStatus,
+    CalDate.toString()
+  );
 
   return (
     <div className="w-full h-[900px] pb-[50px] relative m-auto top-[10px] shadow-[1px_2px_7px_1px_#50515985] rounded-[10px] bg-white">
       <div className=" w-[1025px] m-auto  top-[10px] pb-[30px]">
-        <CalendarComponent />
+        <CommonCalendar
+          viewDrow={viewDrow}
+          CalDate={CalDate}
+          setCalDate={setCalDate}
+        />
       </div>
     </div>
   );
