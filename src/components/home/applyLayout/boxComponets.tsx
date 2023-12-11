2546c841d3cb832adc70e85bdc9b6a4f966c6aa7
hahahaday12 <hahahaday12@naver.com>
2023-12-11 14:30:54 +0900

    Feat: 홈 메인 신청 내역 공통 스타일 컴포넌트

--- src/components/home/applyLayout/boxComponets.tsx
+++ src/components/home/applyLayout/boxComponets.tsx
@@ -4,7 +4,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 
 export function ApplyInfobox({ children, className }: ButtonProps) {
   return (
-    <div className="w-[518px] h-[266px] rounded-[10px] bg-[#ffff] pb-[50px] shadow-[1px_2px_7px_1px_#50515985]">
+    <div className="w-[518px] h-[266px] rounded-[10px] bg-[#e37f7f] pb-[50px] shadow-[1px_2px_7px_1px_#50515985]">
       {children}
     </div>
   );
