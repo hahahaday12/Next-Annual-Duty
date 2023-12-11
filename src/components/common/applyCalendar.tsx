d3eaad8b1e3bb22212f3ef0a246d77246b7c1466
hahahaday12 <hahahaday12@naver.com>
2023-12-11 13:28:17 +0900

    Feat: 연차, 당직 신청 페이지 캘린더 컴포넌트

--- src/components/common/applyCalendar.tsx
+++ src/components/common/applyCalendar.tsx
@@ -1,33 +1,33 @@
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import { DrowItem } from '../application/dayApply';
+import {
+  DrowItem,
+  DutyItemType,
+  AnnualDrowItem,
+} from '../application/dayApply';
 import { DateClickArg } from 'fullcalendar/index.js';
-import { Event } from '../application/dayApply';
 import { MutableRefObject } from 'react';
 
 type ApplyCalendarProps = {
-  handleModalClick: (info: DateClickArg) => void;
-  eventContent: ({ event }: { event: Event }) => JSX.Element;
+  handleModalClick: (info: DateClickArg | Date) => void;
   handleDatesSet: () => void;
-  viewDrow: DrowItem[];
+  viewDrow: DrowItem[] | DutyItemType[] | AnnualDrowItem[];
   calendarRef: MutableRefObject<FullCalendar | null>;
 };
 
 export const CommonApplyCalendar = ({
   handleModalClick,
-  eventContent,
   handleDatesSet,
   viewDrow,
   calendarRef,
 }: ApplyCalendarProps) => {
   return (
-    <div className="w-[1200px] m-auto top-[70px] rounded-[10px] bg-yellow-700 shadow-[1_px_2px_7px_1px_#50515985] relative">
+    <div className="w-[1200px] m-auto top-[70px] rounded-[10px] bg-white shadow-[1px_2px_7px_1px_#50515985] absolute font-LINESeedKRBd">
       <FullCalendar
         plugins={[dayGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
         dateClick={(info) => handleModalClick(info)}
-        eventContent={eventContent}
         datesSet={handleDatesSet}
         ref={calendarRef}
         timeZone="Asia/Seoul"
