861400cb7b9a0cd99b92d89a0fdd5b6e216081cf
hahahaday12 <hahahaday12@naver.com>
2023-12-11 13:20:36 +0900

    Feat: 메인 홈 페이지 코드 수정

--- src/app/main/home/page.tsx
+++ src/app/main/home/page.tsx
@@ -1,111 +1,19 @@
-'use client';
-import { AnnualContainer } from '@/components/home/annual/annual';
-import { DutyContainer } from '@/components/home/duty/duty';
-import { useCallback, useEffect, useState } from 'react';
-import {
-  DeleteAnnualList,
-  DeleteDutyList,
-  MyAnnualList,
-  MyDutyList,
-} from '@/components/api/userInfo';
-import { mainTexts } from '@/constants/mainHome';
 import { CenterBarBox } from '@/components/home/buttonBox/barBox';
 import { AllCalendarList } from '@/components/home/calendar/allCalendar';
+import AllAnnualDuty from '@/components/home/mainHome';
 
 export default function HomeMain() {
-  const [CalDate] = useState<number>(2023);
-  const [annualDataList, setAnnualDataList] = useState([]);
-  const [dutyDataList, setDutyDataList] = useState([]);
-
-  const searchData = useCallback(() => {
-    MyAnnualList(CalDate.toString())
-      .then((data) => {
-        console.log(data.response);
-        const returnDatalist = data.response;
-        setAnnualDataList(returnDatalist);
-        return MyDutyList(CalDate.toString());
-      })
-      .then((data) => {
-        console.log(data.response);
-        const returnDatalist = data.response;
-        setDutyDataList(returnDatalist);
-      })
-      .catch((error) => {
-        console.error('Error', error);
-      });
-  }, [CalDate]);
-
-  useEffect(() => {
-    searchData();
-  }, []);
-
-  const extractDate = (dateString: string) => {
-    if (dateString) {
-      const date = dateString.split('T')[0];
-      return date;
-    }
-    return '';
-  };
-
-  const datalist = useCallback((datalist: any[]) => {
-    const filterViewData = datalist.filter((item: { status: string }) => {
-      return item.status !== 'CANCELLED';
-    });
-    return filterViewData;
-  }, []);
-
-  const deleteButton = useCallback(
-    async (type: string, id: string) => {
-      if (!window.confirm(`${type}를 취소 하시겠습니까?`)) {
-        alert(mainTexts.cancelText);
-        return;
-      }
-
-      try {
-        let deleteFunction;
-        if (type === '연차') {
-          deleteFunction = DeleteAnnualList;
-        } else {
-          deleteFunction = DeleteDutyList;
-        }
-
-        const response = await deleteFunction(id);
-        console.log(response);
-        if (response.status === 200) {
-          alert(`${type}가 취소되었습니다.`);
-          searchData();
-        } else {
-          alert(mainTexts.failCancelText);
-        }
-      } catch (error) {
-        console.error(error);
-        alert(mainTexts.areadyApply);
-      }
-    },
-    [searchData]
-  );
-
   return (
     <>
-      <div className="w-[1060px] h-[1280px] top-[40px] relative font-LINESeedKRBd">
-        <div className="mt-[60px] flex gap-5 content-between">
-          {/* 여기에 연차 컨테이너 */}
-          <AnnualContainer
-            datalist={datalist}
-            annualDataList={annualDataList}
-            extractDate={extractDate}
-            deleteButton={deleteButton}
-          />
-          {/* 여기에 당직 컨테이너*/}
-          <DutyContainer
-            datalist={datalist}
-            dutyDataList={dutyDataList}
-            extractDate={extractDate}
-            deleteButton={deleteButton}
-          />
+      <meta name="description" content="Put your description here."></meta>
+      <div className="relative flex content-center">
+        <div className="h-[1280px] relative font-LINESeedKRBd top-[40px] m-auto bg-fuchsia-500">
+          <div className="w-full mt-[60px] flex">
+            <AllAnnualDuty />
+          </div>
+          <CenterBarBox />
+          <AllCalendarList />
         </div>
-        <CenterBarBox />
-        <AllCalendarList />
       </div>
     </>
   );
