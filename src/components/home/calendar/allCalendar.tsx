7883408f858774d8cf066fb54a238d34562b017e
hahahaday12 <hahahaday12@naver.com>
2023-12-11 15:45:49 +0900

    Feat: 메인 홈 캘린더 컴포넌트

--- src/components/home/calendar/allCalendar.tsx
+++ src/components/home/calendar/allCalendar.tsx
@@ -1,36 +1,69 @@
 'use client';
-import { useMemo, useState } from 'react';
 import { allAnnualList, allDutyList } from '@/components/api/allApis';
 import { getTitleWithStatus } from '@/constants/customStatus';
-import { useCalendarData } from '@/hooks/useCalendarData';
-import useCalendar from '@/hooks/useCalendarLayout';
+import { useEffect, useState } from 'react';
+import { useUpdateDate } from '@/utils/store';
+import { CommonCalendar } from '@/components/common/calendar';
+import {
+  AnnualDrowItem,
+  DutyItemType,
+} from '@/components/application/dayApply';
+import { CombinedDataItem } from '@/hooks/useCalendarData';
 
 export const AllCalendarList = () => {
-  const [CalDate] = useState<number>(2023);
+  const [CalDate, setCalDate] = useState<number>(2023);
+  const [viewDrow, setViewDrow] = useState<CombinedDataItem[]>([]);
+  const { annualDataList, dutyDataList } = useUpdateDate();
 
-  const fetchDataFunction1 = useMemo(
-    () => allAnnualList(CalDate.toString()),
-    [CalDate]
-  );
-  const fetchDataFunction2 = useMemo(
-    () => allDutyList(CalDate.toString()),
-    [CalDate]
-  );
+  useEffect(() => {
+    searchCalendar();
+  }, [CalDate, annualDataList, dutyDataList]);
 
-  const { viewDrow } = useCalendarData(
-    fetchDataFunction1,
-    fetchDataFunction2,
-    getTitleWithStatus,
-    CalDate
-  );
+  const searchCalendar = () => {
+    Promise.all([
+      allAnnualList(CalDate.toString()),
+      allDutyList(CalDate.toString()),
+    ])
+      .then(([annualDataList, dutyDataList]) => {
+        const annualReturnData = annualDataList.response.map(
+          (item: AnnualDrowItem) => ({
+            title: getTitleWithStatus(item),
+            username: item.username,
+            start: new Date(item.startDate).toISOString(),
+            end: new Date(item.endDate).toISOString(),
+            type: 'ANNUAL',
+          })
+        );
 
-  const { CalendarComponent } = useCalendar({ viewDrow });
+        const dutyReturnData = dutyDataList.response.map(
+          (item: DutyItemType) => ({
+            ...item,
+            title: getTitleWithStatus(item),
+            username: item.username,
+            date: new Date(item.dutyDate),
+            type: 'DUTY',
+          })
+        );
+
+        const combinedData = [...annualReturnData, ...dutyReturnData];
+        setViewDrow(combinedData);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  };
 
   return (
-    <div className="w-full h-[870px] pb-[40px] bg-[#ffff] relative top-[30px] rounded-[10px] shadow-[1px_2px_7px_1px_rgba(0,0,0,0.3)]">
-      <div className=" w-[1025px] relative m-auto h-[700px] top-[10px] font-LINESeedKR-Bd pb-[40px]">
-        <CalendarComponent />
+    <>
+      <div className="w-[1200px] h-[1028px] flex m-auto top-[30px] rounded-[10px] pb-[124px]">
+        <div className="w-full h-[980px] pb-[40px] bg-[#ffff] relative rounded-[10px] shadow-[1px_2px_7px_1px_rgba(0,0,0,0.3)] m-auto">
+          <CommonCalendar
+            viewDrow={viewDrow}
+            CalDate={CalDate}
+            setCalDate={setCalDate}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
