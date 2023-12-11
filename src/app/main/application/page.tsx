fc405ec9001e3574fca72fc5c554deb984f09068
hahahaday12 <hahahaday12@naver.com>
2023-12-11 13:19:59 +0900

    Feat: 연차, 당직 신청 페이지

--- src/app/main/application/page.tsx
+++ src/app/main/application/page.tsx
@@ -1,7 +1,17 @@
+import { Topimagesbox } from '@/components/application/topImage';
+import { CommonBarButton } from '@/components/home/buttonBox/button/barButton';
+import { AllApply } from '@/components/application/dayApply';
+
 export default async function Application() {
   return (
-    <>
-      <p>신청 페이지 입니다</p>
-    </>
+    <div className="w-[1200px] pb-[900px] m-auto mt-[100px] relative flex">
+      <div className="w-[1200px] h-[50px] top-[10px] pl-[20px] text-[20px] font-bold absolute">
+        <Topimagesbox />
+        <div className="w-[150px] absolute right-[10px] text-[15px] font-LINESeedKRBd">
+          <CommonBarButton />
+        </div>
+      </div>
+      <AllApply />
+    </div>
   );
 }
